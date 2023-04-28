cf97f2f0208ef96eadfe9dddce702e7e7662ceb5
hugo_milesi <hugogmilesi@gmail.com>
2023-04-28 15:34:18 -0300

    removing ugly expand bars

--- st.py
+++ st.py
@@ -80,108 +80,107 @@ def main():
     st.header('XGBoost Customer Churn Prediction')
     st.markdown("""
                 ## About this project.
-                - This is a visual representation of a data mining project using [telco customer churn dataset](https://www.kaggle.com/datasets/blastchar/telco-customer-churn) stored in kaggle. 
+                - This is a visual representation of a data mining project using [telco customer churn dataset](https://www.kaggle.com/datasets/blastchar/telco-customer-churn) stored at kaggle. 
                 - The model of choosing was **XGBoost**. If you want to checkout how i trained the model, please take a look at this [notebook](https://github.com/hugomilesi/E2E_customer_churn_analysis/blob/main/data_mining.ipynb) here.
                 - The model will predict if a customer will churn based on the data you provide below.
                 """)
     
-    # Expander handler
-    with st.expander("CLICK HERE TO TEST THE MODEL WITH NEW DATA AND SEE THE PREDICTION!"):
-        # divider
-        st.markdown("""<hr style = 'border-top:8px solid crimson; border-radius:5px'>""", unsafe_allow_html=True)
-        
-        st.markdown("""
-                    ## Most relevant fields
-                    - Most relevant fields accordingly to the trained model.
-                    - Information inserted here will have more impact in model's decision than the rest of the fields.
-                    """)
-        
-        contract = st.selectbox(
-            'Customer contract type',
-            ['Month-to-month', 'One year', 'Two year']
+    
+    # divider
+    st.markdown("""<hr style = 'border-top:8px solid crimson; border-radius:5px'>""", unsafe_allow_html=True)
+    
+    st.markdown("""
+                ## Most relevant fields
+                - Most relevant fields accordingly to the trained model.
+                - Information inserted here will have more impact in model's decision than the rest of the fields.
+                """)
+    
+    contract = st.selectbox(
+        'Customer contract type',
+        ['Month-to-month', 'One year', 'Two year']
+    )
+    internetservice = st.selectbox(
+        'Internet service',
+        ['DSL', 'Fiber optic', 'No']
         )
-        internetservice = st.selectbox(
-            'Internet service',
-            ['DSL', 'Fiber optic', 'No']
-            )
-        paymentmethod = st.selectbox(
-            'Customer has payment method?',
-            ['Electronic check', 'Mailed check', 'Bank transfer (automatic)',
-        'Credit card (automatic)']
+    paymentmethod = st.selectbox(
+        'Customer has payment method?',
+        ['Electronic check', 'Mailed check', 'Bank transfer (automatic)',
+    'Credit card (automatic)']
+    )
+    # divider
+    st.markdown("""<hr style = 'border-top:8px solid crimson; border-radius:5px'>""", unsafe_allow_html=True)
+    st.markdown("""## Least Relevant Fields""")
+    col1, col2 = st.columns(2)
+    # column 1
+    with col1:
+        gender = st.selectbox(
+        'Gender',
+        ['Male', 'Female']
         )
-        # divider
-        st.markdown("""<hr style = 'border-top:8px solid crimson; border-radius:5px'>""", unsafe_allow_html=True)
-        st.markdown("""## Least Relevant Fields""")
-        col1, col2 = st.columns(2)
-        # column 1
-        with col1:
-            gender = st.selectbox(
-            'Gender',
-            ['Male', 'Female']
-            )
-            seniorcitizen = st.selectbox(
-            'Customer is Senior?',
-            ['Yes', 'No']
-            )
-            partner = st.selectbox(
-            'Customer has a partner?',
-            ['Yes', 'No']
-            )
-            dependents = st.selectbox(
-            'Customer has a dependents?',
-            ['Yes', 'No']
-            )
-            phoneservice = st.selectbox(
-            'Customer has phone service?',
-            ['Yes', 'No']
-            )
-            multiplelines = st.selectbox(
-            'Customer has multiple lines?',
-            ['No phone service', 'Yes', 'No']
-            )
-            onlinesecurity = st.selectbox(
-            'Customer has online security?',
+        seniorcitizen = st.selectbox(
+        'Customer is Senior?',
+        ['Yes', 'No']
+        )
+        partner = st.selectbox(
+        'Customer has a partner?',
+        ['Yes', 'No']
+        )
+        dependents = st.selectbox(
+        'Customer has a dependents?',
+        ['Yes', 'No']
+        )
+        phoneservice = st.selectbox(
+        'Customer has phone service?',
+        ['Yes', 'No']
+        )
+        multiplelines = st.selectbox(
+        'Customer has multiple lines?',
+        ['No phone service', 'Yes', 'No']
+        )
+        onlinesecurity = st.selectbox(
+        'Customer has online security?',
+        ['No', 'Yes', 'No internet service']
+        )
+        onlinebackup = st.selectbox(
+        'Customer has onlineBackup?',
+        ['No', 'Yes', 'No internet service']
+        )
+    # column 2
+    with col2:
+        deviceprotection = st.selectbox(
+        'Customer has device protection?',
+        ['No', 'Yes', 'No internet service']
+        )
+        devicesupport = st.selectbox(
+            'Customer has device support?',
             ['No', 'Yes', 'No internet service']
             )
-            onlinebackup = st.selectbox(
-            'Customer has onlineBackup?',
+        streamingtv = st.selectbox(
+            'Customer has streaming tv?',
             ['No', 'Yes', 'No internet service']
             )
-        # column 2
-        with col2:
-            deviceprotection = st.selectbox(
-            'Customer has device protection?',
+        streamingmovies = st.selectbox(
+            'Customer has streaming movies benefit?',
             ['No', 'Yes', 'No internet service']
             )
-            devicesupport = st.selectbox(
-                'Customer has device support?',
-                ['No', 'Yes', 'No internet service']
-                )
-            streamingtv = st.selectbox(
-                'Customer has streaming tv?',
-                ['No', 'Yes', 'No internet service']
-                )
-            streamingmovies = st.selectbox(
-                'Customer has streaming movies benefit?',
-                ['No', 'Yes', 'No internet service']
-                )
-            paperlessbilling = st.selectbox(
-                'Customer has paperless billing benefit?',
-                ['No', 'Yes']
-            )
-            tenure = st.number_input('Tenure(months)')
-            monthlycharges = st.number_input('Customer monthly charges.')
-            totalcharges = st.number_input('Customer total charge.')
-        
-        
-        
-        diagnosis = ''
+        paperlessbilling = st.selectbox(
+            'Customer has paperless billing benefit?',
+            ['No', 'Yes']
+        )
+        tenure = st.number_input('Tenure(months)')
+        monthlycharges = st.number_input('Customer monthly charges.')
+        totalcharges = st.number_input('Customer total charge.')
+    
+    
+    
+    diagnosis = ''
     
     #button for prediction
     
     col1, col2, col3, col4, col5 = st.columns(5)
     
-    if col3.button('CUSTOMER DIAGNOSTIC'):
+    if col3.button('CUSTOMER DIAGNOSIS'):
         
         churn_pred = model_prediction([gender, seniorcitizen, partner, dependents, tenure, phoneservice, multiplelines, 
                                 internetservice, onlinesecurity, onlinebackup, deviceprotection, devicesupport, streamingtv,
